99c1a36e72f7247288d99db9184a098f6ccc56e4
Fred <fvinet9@gmail.com>
2024-04-07 20:41:07 +0200

    Update CardShower.php

--- CardShower.php
+++ CardShower.php
@@ -22,18 +22,23 @@ if ($conn) {
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
     <div class="carousel-inner mb-5">
-            <?php foreach ($groupes as $index => $groupe): ?>
-                <div class="carousel-item <?php echo $index == 0 ? 'active' : ''; ?>">
+             <?php foreach ($groupes as $index => $groupe): ?> <!-- Parcours le sous tableau $groupe qui répresente un groupe carte de casse-briques -->
+                 <div class="carousel-item <?php echo $index == 0 ? 'active' : ''; ?>"><!-- Détermine quel élément du carousel est actif -->
                     <div class="container">
                         <div class="row justify-content-center gap-3 mb-5">
-                            <?php foreach ($groupe as $carte): ?>
+                             <?php foreach ($groupe as $carte): ?> <!-- Parcours le talbeau associatif $carte qui représente les données d'une carte de casse-briques dans le groupe en cours d'itération.  -->
                                 <div class="col-md-3 mb-5 d-flex align-items-stretch">
                                     <div class="card bg-dark border-warning-subtle">
+                                        <!-- Affiche l'image de l'utilisateur -->
                                         <img src="<?php echo $carte['CB_Image']; ?>" class="mt-4 ms-3 me-3 rounded-circle d-block mx-auto" alt="...">
                                         <div class="card-body ">
+                                            <!-- Affiche le Nom du casse-brique -->
                                             <h5 class="card-title text-warning text-center"><?php echo $carte['CB_Nom']; ?></h5>
+                                            <!-- Affiche le Nom de créateur du casse brique -->
                                             <p class="card-text text-light">Créateur/rice : <?php echo $carte['CB_Createur']; ?></p>
+                                            <!-- Affiche le type de Casse-brique (Solo ou Multiplayer ou les deux) -->
                                             <p class="card-text text-info"><?php echo $carte['CB_Type']; ?></p>
+                                            <!-- Bouton pour lancer le jeu (Pas encore mis en place) -->
                                             <a href="#"><button class="btn btn-secondary px-3 border-warning-subtle">Launch Game</button></a>
                                         </div>
                                     </div>
